e5017f23cee35b33141aa073afbe40547fe3189f
dantewarriorcp <71193136+dantewarriorcp@users.noreply.github.com>
2020-09-13 10:01:07 -0500

    Add files via upload

---
+++ BackEnd/Models/EstudianteProfesor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class EstudianteProfesor
+    {
+        public int estudianteId { get; set; }
+        public Estudiante Estudiante { get; set; }
+        public int ProfesorId { get; set; }
+        public Profesor Profesor { get; set; }
+    }
+}
+
+}
